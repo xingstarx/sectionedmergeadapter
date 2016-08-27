70296ec8cd161e5ff6bb47ea5bca1e2d1a677943
xingxing <xxx823952375@gmail.com>
2016-08-27 11:53:08 +0800

    format code

--- app/src/main/java/in/abacritt/android/sectionedmergeadapterdemoapp/MainActivity.java
+++ app/src/main/java/in/abacritt/android/sectionedmergeadapterdemoapp/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import in.abacritt.android.sectionedmergeadapterdemoapp.adapter.SectionedMergeAdapter;
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/in/abacritt/android/sectionedmergeadapterdemoapp/adapter/SectionedMergeAdapter.java
+++ app/src/main/java/in/abacritt/android/sectionedmergeadapterdemoapp/adapter/SectionedMergeAdapter.java
@@ -1,15 +1,11 @@
 package in.abacritt.android.sectionedmergeadapterdemoapp.adapter;
 
-import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
+import android.widget.ListAdapter;
 
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 
 public class SectionedMergeAdapter extends MergeAdapter {
@@ -31,34 +27,15 @@ public class SectionedMergeAdapter extends MergeAdapter {
 
     public static class HeaderAdapter extends BaseAdapter {
 
-        private static final int ITEM_TYPE_COUNT = 2;
-        private static final int ITEM_DATA = 0;
-        private static final int ITEM_HEADER = 1;
-        private View headerView;
-<<<<<<< 32294b787376643b597db4e6336044b88b941b3e
-        private List<String> mDatas = new ArrayList<>();
-        private Context mContext;
-
-        public Section(View headerView) {
-            this.headerView = headerView;
-        }
-
-        public Section(Context mContext, View headerView, List<String> mDatas) {
-            this.mContext = mContext;
-=======
-        private ListAdapter adapter;
         private static final int ITEM_HEADER = 0;
         private static final int ITEM_DATA = 1;
         private static final int ITEM_TYPE_COUNT = 2;
+        private View headerView;
+        private ListAdapter adapter;
 
         public HeaderAdapter(View headerView, ListAdapter adapter) {
->>>>>>> fix scroll to bottom crash bug
             this.headerView = headerView;
-            this.mDatas = mDatas;
-        }
-
-        public void setmDatas(List<String> datas) {
-            this.mDatas = datas;
+            this.adapter = adapter;
         }
 
         public View getHeaderView() {
@@ -80,16 +57,7 @@ public class SectionedMergeAdapter extends MergeAdapter {
 
         @Override
         public int getCount() {
-            return mDatas.size() == 0 ? mDatas.size() : mDatas.size() + 1;
-        }
-
-        @Override
-<<<<<<< 32294b787376643b597db4e6336044b88b941b3e
-        public String getItem(int i) {
-            if (i == 0)
-                return null;
-            else
-                return mDatas.get(i - 1);
+            return adapter.getCount() == 0 ? adapter.getCount() : adapter.getCount() + 1;
         }
 
         @Override
@@ -97,19 +65,6 @@ public class SectionedMergeAdapter extends MergeAdapter {
             return position;
         }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (getItemViewType(position) == ITEM_HEADER) {
-                return headerView;
-            } else {
-                if (convertView == null) {
-//                    convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list, parent, false);
-                }
-                String data = getItem(position);
-                TextView textView = (TextView) convertView.findViewById(android.R.id.text1);
-                textView.setText(data);
-                return convertView;
-=======
         public Object getItem(int position) {
             if (position == 0)
                 return null;
@@ -118,45 +73,21 @@ public class SectionedMergeAdapter extends MergeAdapter {
         }
 
         @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            if (position == 0) {
-                return ITEM_HEADER;
-            }
-            return ITEM_DATA;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return ITEM_TYPE_COUNT;
-        }
-
-        @Override
         public View getView(int position, View view, ViewGroup viewGroup) {
             if (getItemViewType(position) == ITEM_HEADER) {
                 return headerView;
             } else {
                 return adapter.getView(position - 1, view, viewGroup);
->>>>>>> fix scroll to bottom crash bug
             }
         }
 
         @Override
         public boolean isEnabled(int position) {
-            if (getItemViewType(position) == 0)
+            if (getItemViewType(position) == 0) {
                 return false;
-<<<<<<< 32294b787376643b597db4e6336044b88b941b3e
-            else
-                return true;
-=======
-            else {
+            } else {
                 return adapter.isEnabled(position - 1);
             }
->>>>>>> fix scroll to bottom crash bug
         }
     }
 
